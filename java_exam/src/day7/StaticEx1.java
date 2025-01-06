951e7dc07f4e28ad9919a9b2ca69e37c232f6fc0
tj-bu-708-teacher <tj-bu-708-teacher@DESKTOP-IBLBVFI>
2025-01-06 13:20:47 +0900

    2025.01.06 클래스, 패키지, static

---
+++ java_exam/src/day7/StaticEx1.java
@@ -0,0 +1,15 @@
+package day7;
+
+public class StaticEx1 {
+	int num = 10;
+	int money = 50000;
+	static final double PI = 3.141592;
+	
+	int sum(int x, int y) {
+		return x+y;
+	}
+	
+	static int staticSum(int x, int y) {
+		return x+y;
+	}
+}
